c50d60ba3f5dc8ace39984d9fbcffe3c25b0fde3
维章 <unclerangoo@gmail.com>
2022-08-11 16:17:47 +0800

    modify test.sql

--- rsqldb-disk/client/test.sql
+++ rsqldb-disk/client/test.sql
@@ -1,66 +1,76 @@
--- 源表1
-DROP TABLE ticket;
-CREATE TABLE ticket (
-                        id BIGINT,
-                        perform_id BIGINT,
-                        position VARCHAR(50),
-                        gmt_modified TIMESTAMP,
-                        primary key (id)
-);
-INSERT INTO ticket VALUES (5, 2, "r2c2", NOW());
-UPDATE ticket SET position = "r2c4" WHERE id = 5;
-DELETE FROM ticket WHERE id = 5;
-
--- 源表2
-DROP TABLE odeum;
-CREATE TABLE odeum (
-                       id BIGINT,
-                       name VARCHAR(50),
-                       gmt_modified TIMESTAMP,
-                       primary key (id)
-);
-INSERT INTO odeum VALUES (2, "B", NOW());
-
--- 源表3
-DROP TABLE perform;
-CREATE TABLE perform (
-                         id BIGINT,
-                         name VARCHAR(50),
-                         odeum_id BIGINT,
-                         gmt_modified TIMESTAMP,
-                         primary key (id)
-);
-INSERT INTO perform VALUES (2, "b", 2, NOW());
-UPDATE perform SET name = "c" WHERE id = 1;
-DELETE FROM perform WHERE id = 2;
-
--- 目标表
-DROP TABLE test_cdc_sync;
-CREATE TABLE test_cdc_sync (
-                               ticket_id BIGINT,
-                               position VARCHAR(50),
-                               odeum_name VARCHAR(50),
-                               perform_name VARCHAR(50),
-                               primary key (ticket_id)
-);
-SELECT * FROM test_cdc_sync;
+CREATE TABLE ticket
+(
+    `id`           BIGINT,
+    `perform_id`   BIGINT,
+    `position`     VARCHAR,
+    `gmt_modified` TIMESTAMP,
+    primary key (id)
+) WITH (
+      type = 'rocketmq',
+      topic = 'rsqldb-ticket',
+      groupName = 'rsqldb-ticket',
+      namesrvAddr = '127.0.0.1:9876',
+      isJsonData = 'true',
+      msgIsJsonArray = 'false'
+      );
 
 
-CREATE VIEW ticket_perform AS SELECT
-                                  t.id AS ticket_id,
-                                  t.`position` AS `position`,
-                                  p.name AS perform_name,
-                                  p.odeum_id AS odeum_id
-                              FROM ticket AS t
-                                       JOIN perform FOR SYSTEM_TIME AS OF PROCTIME() AS p
-                                            ON t.perform_id = p.id;
+CREATE TABLE odeum
+(
+    `id`           BIGINT,
+    `name`         VARCHAR,
+    `gmt_modified` TIMESTAMP,
+    primary key (id)
+) WITH (
+      type = 'rocketmq',
+      topic = 'rsqldb-odeum',
+      groupName = 'rsqldb-odeum',
+      namesrvAddr = '127.0.0.1:9876',
+      isJsonData = 'true',
+      msgIsJsonArray = 'false'
+      );
 
-INSERT INTO test_cdc_sync SELECT
-                              t.ticket_id AS ticket_id,
-                              t.`position` AS `position`,
-                              o.name AS odeum_name,
-                              t.perform_name AS perform_name
+CREATE TABLE perform
+(
+    `id`           BIGINT,
+    `name`         VARCHAR,
+    `odeum_id`     BIGINT,
+    `gmt_modified` TIMESTAMP,
+    primary key (id)
+) WITH (
+      type = 'rocketmq',
+      topic = 'rsqldb-perform',
+      groupName = 'rsqldb-perform',
+      namesrvAddr = '127.0.0.1:9876',
+      isJsonData = 'true',
+      msgIsJsonArray = 'false'
+      );
+
+CREATE TABLE test_cdc_sync
+(
+    `ticket_id`    BIGINT,
+    `position`     VARCHAR,
+    `odeum_name`   VARCHAR,
+    `perform_name` VARCHAR,
+    primary key (ticket_id)
+) WITH (
+      type = 'print'
+      );
+
+CREATE VIEW ticket_perform AS
+SELECT t.id         AS ticket_id,
+       t.`position`   AS `position`,
+       p.name       AS perform_name,
+       p.odeum_id   AS odeum_id
+FROM ticket AS t
+         JOIN perform FOR SYSTEM_TIME AS OF PROCTIME() AS p ON t.perform_id = p.id;
+
+INSERT INTO test_cdc_sync
+SELECT t.ticket_id    AS ticket_id,
+       t.`position`     AS `position`,
+       o.name         AS odeum_name,
+       t.perform_name AS perform_name
 FROM ticket_perform AS t
-         JOIN odeum FOR SYSTEM_TIME AS OF PROCTIME() AS o
-              ON t.odeum_id = o.id;
+         JOIN odeum FOR SYSTEM_TIME AS OF PROCTIME() AS o ON t.odeum_id = o.id;
+
 
